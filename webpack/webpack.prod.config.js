cd683200ba1346872d8838b0c4551856266ca4ba
Pele Odiase <pele.odiase@dugout.com>
2018-02-01 11:02:59 +0000

    Initial commit

---
+++ webpack/webpack.prod.config.js
@@ -0,0 +1,34 @@
+require('dotenv').config({silent: true});
+
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, '../build');
+const APP_DIR = path.resolve(__dirname, '../src');
+
+const config = {
+    entry: APP_DIR + '/index.js',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loaders: "babel-loader",
+                query: {
+                    cacheDirectory: true,
+                    presets: ['react', 'es2015'],
+                },
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+        ]
+    }
+};
+
+module.exports = config;
